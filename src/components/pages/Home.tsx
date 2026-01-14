ced4070a31c7b653916a845763e8467acc151dcf
sultansyah <muhammadsultansyah14@gmail.com>
2026-01-14 23:16:34 +0700

    feat: add patners section

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../layout/Navbar";
 import HeroSection from "../sections/HeroSection";
+import PatnersSection from "../sections/PatnersSection";
 
 export default function Home() {
     return (
         <div className="min-h-screen bg-[#F1F4F5] font-poppins">
             <Navbar />
             <HeroSection />
+            <PatnersSection />
         </div>
     );
 }
